f38c2f942a5642338ca012aadfe43b97edab3d1c
Theoo2B <theophile.venturini@edu.ece.fr>
2021-12-06 20:00:10 +0100

    master

--- js/script.js
+++ js/script.js
@@ -12,16 +12,38 @@ function start() {
       const data = response.data;
 
       // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      for (var i = 0; i < 4; i++) 
+      {
+        const main[i] = data.weather[i].main;
+        const description[i] = data.weather[i].description;
+        const temp[i] = data.main[i].temp;
+        const icon[i] = apiWeather.getHTMLElementFromIcon(data.weather[i].icon);
+      }
+      
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      
+      document.getElementById('today-forecast-main').innerHTML = main[0];
+      document.getElementById('today-forecast-more-info').innerHTML = description[0];
+      document.getElementById('icon-weather-container').innerHTML = icon[0];
+      document.getElementById('today-forecast-temp').innerHTML = `${temp[0]}°C`;
+
+      document.getElementById('tomorrow-forecast-main').innerHTML = main[1];
+      document.getElementById('tomorrow-forecast-more-info').innerHTML = description[0];
+      document.getElementById('icon-weather-container').innerHTML = icon[1];
+      document.getElementById('tomorrow-forecast-temp').innerHTML = `${temp[1]}°C`;
+
+      document.getElementById('ATommorow-forecast-main').innerHTML = main[2];
+      document.getElementById('ATommorow-forecast-more-info').innerHTML = description[2];
+      document.getElementById('icon-weather-container').innerHTML = icon[2];
+      document.getElementById('ATommorow-forecast-temp').innerHTML = `${temp[2]}°C`;
+
+      document.getElementById('AATommorrow-forecast-main').innerHTML = main[3];
+      document.getElementById('AATommorrow-forecast-more-info').innerHTML = description[3];
+      document.getElementById('icon-weather-container').innerHTML = icon[3];
+      document.getElementById('AATommorrow-forecast-temp').innerHTML = `${temp[3]}°C`;
+
+
+
       
     })
     .catch(function(error) {
